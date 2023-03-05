3045bbcad3f0915d8421a07b792bed5c5fd1e3b6
Brian <brian.salkas@gmail.com>
2023-03-05 16:16:17 -0600

    rename setup.py to setup.sh

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 #import create_jsonl
 subreddits = st.text_input('enter subreddits seperated by spaces')
-#if st.button('get jsonl'):
-#    st.write(f'creating jsonl from subreddits: {subreddits}, this may take some time')
-#    st.text(create_jsonl.create(subreddits))
+if st.button('get jsonl'):
+    st.write(f'creating jsonl from subreddits: {subreddits}, this may take some time')
+    st.text(create_jsonl.create(subreddits))
 
