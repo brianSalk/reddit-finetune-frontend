e6b6c9576de50ebe29d986e49dd24fd0fefca6e5
Brian <brian.salkas@gmail.com>
2023-03-05 15:52:52 -0600

    rename app

---
+++ setup.py
@@ -0,0 +1,12 @@
+mkdir -p ~/.streamlit/            
+echo "                            
+[general]                         
+email = \"brian.salkas@gmail.com\"
+" > ~/.streamlit/credentials.toml 
+echo "                            
+[server]                          
+headless = true                   
+enableCORS=false                  
+port = $PORT                      
+" > ~/.streamlit/config.toml      
+
